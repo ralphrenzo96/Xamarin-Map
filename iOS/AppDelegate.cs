d71c189d9f2d4ac0d3277603542aac2c7ad01969
Ralph Renzo Pitogo <ralph.pitogo@appstone.tech>
2017-10-06 15:38:42 +0800

    Xamarin Map

---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace xamarinmap.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new App());
+
+            // TODO : Map Initialization
+            Xamarin.FormsMaps.Init();
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
---
+++ xamarinmap/Views/MapPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace xamarinmap.Views
+{
+    public partial class MapPage : ContentPage
+    {
+        bool isMapLoaded;
+
+        public MapPage()
+        {
+            InitializeComponent();
+
+            myMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(10.333928, 123.934259), Distance.FromMiles(0.3)));
+            isMapLoaded = true;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+			myMap.MapType = MapType.Street;
+        }
+
+        void SliderValue_Changed(Object sender, ValueChangedEventArgs e)
+        {
+            if (isMapLoaded)
+            {
+                var zoomLevel = e.NewValue; // Between 1 and 18
+                var latlongDegrees = 360 / (Math.Pow(2, zoomLevel));
+                myMap.MoveToRegion(new MapSpan(myMap.VisibleRegion.Center, latlongDegrees, latlongDegrees));
+            }
+        }
+    }
+}
